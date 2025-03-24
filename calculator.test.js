62f5077a923abb714d736311f36c6d7dcf289178
Mukul Kaushik <mukul.kaushik@Incture.local>
2025-03-25 01:13:08 +0530

    Written test case for sum of multiple comma-separated numbers

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
- const { add } = require("./calculator");
+const { add } = require("./calculator");
 
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
@@ -11,4 +11,8 @@ test("returns the number itself when a single number is provided", () => {
 test("returns sum of two comma-separated numbers", () => {
     expect(add("1,2")).toBe(3);
   });
+test("returns sum of multiple comma-separated numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+  
   
