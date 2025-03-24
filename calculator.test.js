fa056d65ff08d31c73f658b1a626030cfb8d130a
Mukul Kaushik <mukul.kaushik@Incture.local>
2025-03-25 00:36:40 +0530

    Written test case to return the number itself for a single input

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,8 @@
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number itself when a single number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
+  
